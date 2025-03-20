2aaa41b21d5eabdb06aaae20406983be66b8b97f
lightmanLP <liteman1000@gmail.com>
2025-03-21 02:38:51 +0700

    fix

--- src/graphic/Fast3D/gfx_dxgi.cpp
+++ src/graphic/Fast3D/gfx_dxgi.cpp
@@ -102,6 +102,7 @@ static struct {
     bool in_focus;
     RAWINPUTDEVICE raw_input_device[1];
     POINT raw_mouse_delta_buf;
+    POINT prev_mouse_cursor_pos;
 
     void (*on_fullscreen_changed)(bool is_now_fullscreen);
     bool (*on_key_down)(int scancode);
@@ -468,6 +469,8 @@ static LRESULT CALLBACK gfx_dxgi_wnd_proc(HWND h_wnd, UINT message, WPARAM w_par
             break;
         case WM_KILLFOCUS:
             dxgi.in_focus = false;
+            dxgi.prev_mouse_cursor_pos.x = 0;
+            dxgi.prev_mouse_cursor_pos.y = 0;
             break;
         default:
             return DefWindowProcW(h_wnd, message, w_param, l_param);
@@ -610,13 +613,12 @@ static void gfx_dxgi_get_mouse_delta(int32_t* x, int32_t* y) {
         dxgi.raw_mouse_delta_buf.x = 0;
         dxgi.raw_mouse_delta_buf.y = 0;
     } else {
-        static int32_t prev_x = 0, prev_y = 0;
         int32_t current_x, current_y;
         gfx_dxgi_get_mouse_pos(&current_x, &current_y);
-        *x = current_x - prev_x;
-        *y = current_y - prev_y;
-        prev_x = current_x;
-        prev_y = current_y;
+        *x = current_x - dxgi.prev_mouse_cursor_pos.x;
+        *y = current_y - dxgi.prev_mouse_cursor_pos.y;
+        dxgi.prev_mouse_cursor_pos.x = current_x;
+        dxgi.prev_mouse_cursor_pos.y = current_y;
     }
 }
 
